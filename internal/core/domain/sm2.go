a8d0b276c94b562d92f811b3622ac9a9e4a07eb1
Gabriel Caldas <gcaldasl2000@gmail.com>
2024-12-30 15:43:40 -0300

    refactor: break sm2 algorithm in small functions

--- internal/core/domain/sm2.go
+++ internal/core/domain/sm2.go
@@ -10,22 +10,35 @@ func NewSM2Calculator() *SM2Calculator {
 
 func (s *SM2Calculator) Calculate(card *Card, quality int) {
 	if quality >= 3 {
-		if card.Interval == 0 {
-			card.Interval = 1
-		} else if card.Interval == 1 {
-			card.Interval = 6
-		} else {
-			card.Interval = int(float64(card.Interval) * card.EaseFactor)
-		}
+		card.Interval = s.calculateInterval(card)
 	} else {
 		card.Interval = 1
 	}
 
-	card.EaseFactor = card.EaseFactor + (0.1 - (5-float64(quality))*(0.08+(5-float64(quality))*0.02))
-	if card.EaseFactor < 1.3 {
-		card.EaseFactor = 1.3
-	}
+	card.EaseFactor = s.calculateEaseFactor(card.EaseFactor, quality)
+	s.updateReviewDates(card)
+}
 
+func (s *SM2Calculator) calculateInterval(card *Card) int {
+	switch card.Interval {
+	case 0:
+		return 1
+	case 1:
+		return 6
+	default:
+		return int(float64(card.Interval) * card.EaseFactor)
+	}
+}
+
+func (s *SM2Calculator) calculateEaseFactor(currentEF float64, quality int) float64 {
+	newEF := currentEF + (0.1 - (5-float64(quality))*(0.08+(5-float64(quality))*0.02))
+	if newEF < 1.3 {
+		return 1.3
+	}
+	return newEF
+}
+
+func (s *SM2Calculator) updateReviewDates(card *Card) {
 	card.LastReviewed = time.Now()
 	card.NextReview = time.Now().AddDate(0, 0, card.Interval)
 }
